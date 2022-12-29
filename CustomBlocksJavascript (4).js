4735264a0d1dc6a5219cb98bf36205c5f34d9ac9
katatsumuri-programming <programmingkatatsumuri@gmail.com>
2022-12-29 17:56:53 +0900

    不要なファイルを削除

--- CustomBlocksJavascript (4).js
+++
@@ -1,6 +0,0 @@
-Blockly.JavaScript['output'] = function(block) {
-  var value_output = Blockly.JavaScript.valueToCode(block, 'output', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
