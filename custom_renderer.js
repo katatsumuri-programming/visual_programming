4680e7a8a37c33a3314d170678b22dfc3ddca1cd
katatsumuri-programming <programmingkatatsumuri@gmail.com>
2023-05-01 20:08:23 +0900

    不要なファイルを削除

--- custom_renderer.js
+++
@@ -1,6 +0,0 @@
-CustomRenderer = new function(name) {
-    CustomRenderer.superClass_.constructor.call(this, name);
-  };
-  Blockly.utils.object.inherits(CustomRenderer,
-      Blockly.blockRendering.Renderer);
-Blockly.blockRendering.register('custom_renderer', CustomRenderer);
